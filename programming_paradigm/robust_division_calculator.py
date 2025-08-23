d99374b22dcb59cfc13433000da21d4190927b6f
KhaledT5 <natorukh12345@gmail.com>
2025-08-23 08:56:39 +0300

    Add a division calc with error measures

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,12 @@
+def safe_divide(numerator, denominator):
+    try:
+        num = float(numerator)
+        den = float(denominator)
+        
+        try:
+            result = num / den
+            return f"The result of the division is {result}"
+        except ZeroDivisionError:
+            return f"Error: cannot divide by zero."
+    except ValueError:
+        return f"Error: Please enter numeric values only."    
